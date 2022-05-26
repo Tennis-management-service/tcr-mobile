dd98e3ffa6fe67bbe42cc4b0e18635ff559d9fbe
Spyros <spyridonkaperonis@gmail.com>
2022-05-25 23:37:37 -0400

    coach and student folders added. in addition, components folder added within these two folders.

---
+++ src/student/components/Search_screen.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Search_screen = () => {
+
+    // write functionality for search.
+    // that screen will lead to another screen showing the results of the search. 
+
+    
+}
+
+export default Search_screen;
