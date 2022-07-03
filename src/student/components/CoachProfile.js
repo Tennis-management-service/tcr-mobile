7a15fd3f81ca26b2ec054be9fe869d06d86201c7
Spyros <spyridonkaperonis@gmail.com>
2022-07-03 12:17:32 -0400

    component to render coach profile.
    works well. It needs to be populated with more data

--- src/student/components/CoachProfile.js
+++ src/student/components/CoachProfile.js
@@ -2,12 +2,13 @@ import React from "react";
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
 
-export const CoachProfile = ({navigation}, props) => {
-
+export const CoachProfile = ({route, navigation}) => {
+    const {name, email} = route.params;
 
     return(
         <View>
-            <Text>This is Coach {props.name}</Text>
+            <Text>This is Coach {name}</Text>
+            <Text>This is my email: {email}</Text>
         </View>
     )
 }
