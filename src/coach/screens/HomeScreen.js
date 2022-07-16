fba1be10d69e779cafc5fd0245f1c14b6ab6a23a
Spyros <spyridonkaperonis@gmail.com>
2022-07-16 19:49:29 -0400

    changes arrow function naming

--- src/coach/screens/HomeScreen.js
+++ src/coach/screens/HomeScreen.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
-import  {CalendarSlider}  from '../components/calendar'
+import  {CalendarComponent}  from '../components/calendar'
 
 export const HomeScreen = () => {
   return (
     <View>
-        <CalendarSlider/>
+        <CalendarComponent/>
     </View>
   )
 }
