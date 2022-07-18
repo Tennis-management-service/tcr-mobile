f5fc3660dfbedd25ae11c702ab52179337fc2c6c
Spyros <spyridonkaperonis@gmail.com>
2022-07-18 13:28:43 -0400

    getMarkedDates() works. Next step is to create a function for items={}

--- src/coach/components/calendar.js
+++ src/coach/components/calendar.js
@@ -128,31 +128,15 @@
 
 
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Calendar, CalendarList, Agenda} from 'react-native-calendars';
-import {format, startOfMonth, addYears} from 'date-fns';
-import { color } from 'react-native-reanimated';
+import { StyleSheet, View } from 'react-native';
+import { Agenda} from 'react-native-calendars';
+import {format } from 'date-fns';
+
 
 
 const formatt = (date) => format(date, 'yyyy-MM-dd');
-const getMarkedDates = (baseDate, appointments) => {
-  const markedDates = {};
 
-  markedDates[formatt(baseDate)] = { selected: true };
-
-  appointments.forEach((appointment) => {
-    const formattedDate = formatt(new Date(appointment.date));
-    markedDates[formattedDate] = {
-      ...markedDates[formattedDate],
-      marked: true,
-    };
-  });
-
-  return markedDates;
-};
-
-
-const getMarkedDatess = (appointmentss) => {
+const getMarkedDates = (appointmentss) => {
 
     const markedDates = {};
 
@@ -177,22 +161,25 @@ export const CalendarComponent = () => {
    
   const APPOINTMENTS = [
     {
-      date: '2022-06-13T05:00:00.000Z',
+      date: '2022-07-13T05:00:00.000Z',
       title: "It's a past thing!",
     },
     {
-      date: '2022-06-15T05:00:00.000Z',
+      date: '2022-07-15T05:00:00.000Z',
       title: "It's a today thing!",
     },
     {
-      date: '2022-06-18T05:00:00.000Z',
+      date: '2022-07-18T05:00:00.000Z',
       title: "It's a future thing!",
     },
+    {
+        date: '2022-07-19T05:00:00.000Z'
+    }
   ];
 
-  console.log(getMarkedDatess(APPOINTMENTS));
+  console.log(getMarkedDates(APPOINTMENTS));
 
-  const markedDates = getMarkedDatess(APPOINTMENTS);
+  const markedDates = getMarkedDates(APPOINTMENTS);
   return (
     <View style={styles.container}>
       <Agenda
@@ -200,7 +187,8 @@ export const CalendarComponent = () => {
         items={{
             '2022-07-13': [{name: 'item1'}, {name: 'Item1.1'}],
             '2022-07-13': [{name: 'item1'}, {name: 'Item1.1'}],
-            '2022-07-18': [{name: 'item2', height: 80}]
+            '2022-07-18': [{name: 'item2', height: 80}],
+            '2022-07-19': [{name: 'item4'}]
         }}
         loadItemsForMonth={(month) => {console.log('trigger items loading')}}
         minDate={'2022-01-15'}
