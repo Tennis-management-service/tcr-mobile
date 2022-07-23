656f8aa1dc97b33f8ac77801b46f325c1ced34c2
Spyros <spyridonkaperonis@gmail.com>
2022-07-23 16:39:44 -0400

    changes naming in CalendarScreen

---
+++ src/coach/screens/CalendarScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+import  {CalendarComponent}  from '../components/calendar'
+
+export const CalendarScreen = () => {
+  return (
+    <View>
+        <CalendarComponent/>
+    </View>
+  )
+}
