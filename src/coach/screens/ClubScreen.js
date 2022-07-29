8d72aec4a2effb692b08c8b83a08cef73f9bbc81
Spyros <spyridonkaperonis@gmail.com>
2022-07-28 20:26:28 -0400

    adds if statement for club or no club

--- src/coach/screens/ClubScreen.js
+++ src/coach/screens/ClubScreen.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Text, View } from 'react-native'
 
-export const ClubScreen = () => {
+
+{/**\
+FUNCTIONS WE NEED
+  * Function to render if coach has assigned club,
+  * Function to render club
+
+
+*/}
+
+const ClubAssigned = () => {
   return (
     <View>
-        <Text>This is the Club Screen</Text>
+      <Text>Club</Text>
     </View>
   )
+}
+const ClubNotAssigned = () => {
+  return (
+    <View>
+      <Text>Club</Text>
+    </View>
+  )
+}
+
+export const ClubScreen = ({props}) => {
+  const [club, setClub] = useState(true);
+  setClub(true);
+
+  switch(club){
+    case true:
+       <ClubAssigned />
+    break;
+    case false:
+       <ClubNotAssigned />
+    break;
+    default:
+      return null;
+  }
 }
