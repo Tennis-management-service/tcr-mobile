4ba8d93a046a59dd2b498013ec726d13953a3510
Spyros <spyridonkaperonis@gmail.com>
2022-07-30 10:39:00 -0400

    adds two different components for when there is a club and when there is not a club

---
+++ src/coach/components/clubNotAssigned.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {View, Text} from 'react-native';
+
+export const ClubNotAssigned = () => {
+    return (
+      <View>
+        <Text>Club</Text>
+      </View>
+    )
+}
