25538f28e57a08671ce6a6d0a46ca6d32ea42cbb
Spyros <spyridonkaperonis@gmail.com>
2022-07-30 14:44:23 -0400

    adds clubNotAssigned.js

--- src/coach/components/clubNotAssigned.js
+++ src/coach/components/clubNotAssigned.js
@@ -1,10 +1,45 @@
 import React from "react";
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
-export const ClubNotAssigned = () => {
+export const ClubNotAssigned = ({props, nav}) => {
     return (
-      <View>
-        <Text>Not Club</Text>
+      <View style={styles.container}>
+        <TouchableOpacity 
+          style={styles.touchableOpacity}
+          onPress={() => {
+            nav.navigate('Add Club');
+          }}>
+          <Text style={styles.text}>Add Club + </Text>
+        </TouchableOpacity>
       </View>
     )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    height: '100%',
+    flexDirection: 'column',
+
+    
+  },
+  touchableOpacity:{
+    backgroundColor: 'green',
+
+    //height
+    height: "10%",
+
+    //margin
+    margin: "4%",
+
+    //contentPlacement
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    //container design
+    borderRadius: 10,
+  }, 
+  text: {
+    color: "white",
+  }
+})
