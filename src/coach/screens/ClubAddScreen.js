d5ef0840c08d783ca6ae8a415f862c5024e1fea2
Spyros <spyridonkaperonis@gmail.com>
2022-07-30 14:43:42 -0400

    adds ClubAddScreen.js

---
+++ src/coach/screens/ClubAddScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+
+export const ClubAddScreen = () => {
+  return (
+    <View>
+        <Text>Add Club</Text>
+    </View>
+  )
+}
