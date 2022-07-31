3305c177774f152ab0b28a63c8f79983ce9c1c75
Spyros <spyridonkaperonis@gmail.com>
2022-07-31 14:49:05 -0400

    inserts components into ClubAddScreen.js

--- src/coach/screens/ClubAddScreen.js
+++ src/coach/screens/ClubAddScreen.js
@@ -1,17 +1,45 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
+import { ClubAddRenderBox } from '../components/clubAddRenderBox';
 
 export const ClubAddScreen = () => {
   return (
     <View>
-        <Text>Add Club</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Club Name, Zip Code, Area"
+          autoFocus={true}
+          blurOnSubmit={true}
+          contextMenuHidden={true}
+          
+        />
+        <ClubAddRenderBox/>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
     container: {
-
+      flex: 1,
+      flexDirection: 'column',
     },
+    input: {
+      backgroundColor: 'yellow',
+      fontSize: 18,
+      
+
+      
+      
+      //Placement
+      marginTop: "2%",
+      marginRight: "2%",
+      marginLeft: "2%",
+      marginBottom: "4%",
+
+      //Border
+      borderWidth: 0.6,
+      borderColor: 'blue',
+      borderRadius: 5,
+    }
     
 })
