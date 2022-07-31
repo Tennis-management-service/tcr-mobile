e311b456f57f4ff68050a0123c23aed5318781fc
Spyros <spyridonkaperonis@gmail.com>
2022-07-31 14:49:49 -0400

    adds component for rendering search results for clubs

---
+++ src/coach/components/clubAddRenderBox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+export const ClubAddRenderBox = () => {
+  return (
+    <View style={styles.container}>
+        <FlatList />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        //Border
+        borderWidth: 1,
+        height: "86%",
+    }
+})
