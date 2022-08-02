04ceaab7fd0365fe2f8c451585a2512d6a1b0961
Spyros <spyridonkaperonis@gmail.com>
2022-08-01 21:22:21 -0400

    adds flatlist in flatlistImageSlider.js

--- src/coach/components/flatlistImageSlider.js
+++ src/coach/components/flatlistImageSlider.js
@@ -1,71 +1,90 @@
 import React from 'react'
-import { View, Text, StyleSheet, LayoutAnimation } from 'react-native';
+import { View, Text, StyleSheet, LayoutAnimation, FlatList } from 'react-native';
 
 export const FlatListImageSlider = ({props}) => {
-  return (
-    <View>
-        <Text>Slider</Text>
-        <Text>Tab Bar</Text>
-    </View>
-  )
-}
 
+    onViewableItemsChanged = ({viewableItems, changed}) => {
+        if(viewableItems.length > 0){
+            let currentIndex = viewableItems[0].index;
+            if(
+                currentIndex % props.data.length === props.data.length -1 &&
+                props.loop)
+                {
+                    //Change this to useState
+                    this.setState({
+                        index: currentIndex,
+                        data: [...this.state.data, ...this.props.data],
+                    });
+                } else {
+                    //Change this to useState
+                    this.setState({index: currentIndex});
+                }
 
-onViewableItemsChanged = ({viewableItems, changed}) => {
-    if(viewableItems.length > 0){
-        let currentIndex = viewableItems[0].index;
-        if(
-            currentIndex % props.data.length === props.data.length -1 &&
-            props.loop)
-            {
-                //Change this to useState
-                this.setState({
-                    index: currentIndex,
-                    data: [...this.state.data, ...this.props.data],
-                });
-            } else {
-                //Change this to useState
-                this.setState({index: currentIndex});
-            }
+                if (props.currentIndexCallback) {
+                    props.currentIndexCallback(currentIndex);
+                }
+        }
+    };
 
-            if (props.currentIndexCallback) {
-                props.currentIndexCallback(currentIndex);
-            }
+    viewabilityConfig = {
+        viewAreaCoveragePercentThreshold: 50,
+    };
+
+    componentWillUnmount = () => {
+        if(props.autoscroll){
+            this.stopAutoPlay();
+        }
+    };
+
+    startAutoPlay = () => {
+        props.sliderTimer = setInterval(
+            this.changeSliderListIndex,
+            this.props.timer,
+        );
+    };
+
+    stopAutoPlay = () => {
+        if(props.sliderTimer){
+            clearInterval(props.sliderTimer);
+            props.sliderTimer = null;
+        }
+    };
+
+    changeSliderListIndex = () => {
+        if(props.animation){
+            LayoutAnimation.configureNext(LayoutAnimation.Presets.easeIn);
+        }
+        //Change this to useState
+        this.setState({index: this.state.index+1});
+        this.slider.current.scrollToIndex({
+            index: this.state.index,
+            animated: true,
+        })
     }
-};
 
-viewabilityConfig = {
-    viewAreaCoveragePercentThreshold: 50,
-};
 
-componentWillUnmount = () => {
-    if(props.autoscroll){
-        this.stopAutoPlay();
-    }
-};
+    const itemWidth = props.width;
+    const separatorWidth = props.width;
+    totalItemWidth = itemWidth + separatorWidth;
 
-startAutoPlay = () => {
-    this.sliderTimer = setInterval(
-        this.changeSliderListIndex,
-        this.props.timer,
-    );
-};
+    return (
+        <View>
+            <FlatList
 
-stopAutoPlay = () => {
-    if(this.sliderTimer){
-        clearInterval(this.sliderTimer);
-        this.sliderTimer = null;
-    }
-};
+                getItemLayout={(data, index) => ({
+                    length: totalItemWidth,
+                    offset: totalItemWidth * index,
+                    index,
+                })}
 
-changeSliderListIndex = () => {
-    if(props.animation){
-        LayoutAnimation.configureNext(LayoutAnimation.Presets.easeIn);
-    }
-    //Change this to useState
-    this.setState({index: this.state.index+1});
-    this.slider.current.scrollToIndex({
-        index: this.state.index,
-        animated: true,
-    })
+                snapToInterval={totalItemWidth}
+
+                decelerationRate = "fast"
+
+                bounces={false}
+
+            />
+        </View>
+      )
+
 }
