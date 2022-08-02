cfb5424cb3a4aed06c9404c321454aa7a92b4d00
Spyros <spyridonkaperonis@gmail.com>
2022-08-01 21:19:51 -0400

    adds style for view in clubAssigned.js

--- src/coach/components/clubAssigned.js
+++ src/coach/components/clubAssigned.js
@@ -1,10 +1,18 @@
 import React from "react"
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 export const ClubAssigned = () => {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Club</Text>
       </View>
     )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    flexDirection: 'column',
+    
+  }
+});
