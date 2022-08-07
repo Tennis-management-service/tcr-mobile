400abad7002c5001300811cdb1bd85f85f94cf5f
Spyros <spyridonkaperonis@gmail.com>
2022-08-07 17:14:46 -0400

    adds working input field for new posts from coach

--- src/coach/components/clubPostInput.js
+++ src/coach/components/clubPostInput.js
@@ -1,10 +1,49 @@
-import React from 'react'
-import {View, Text, StyleSheet, TextInput} from 'react-native'
+import React, {useState} from 'react'
+import {View, Text, StyleSheet, TextInput, Button} from 'react-native'
 
-export const ClubPostInput = () => {
+
+
+
+
+export const ClubPostInput = ({props}) => {
+    const [text, setText] = useState('');
+    const [height1, setHeight] = useState();
+    
+    //Gets bigger value of height and returns it. 
+    const bestHeight = Math.max(120, height1);
+
   return (
-    <View>
-        <TextInput/>
+    <View style={[styles.container,{width: props.screenWidth, height: 150}]}>
+        <TextInput
+            style={[styles.input, {height: 100}]}
+            value={text}
+            multiline={true}
+            onChangeText={(text) => setText(text)}
+            onContentSizeChange={(event) => {
+                const height = event.nativeEvent.contentSize.height;
+                console.log(height);
+                setHeight(height);
+            }}
+            placeholder={'Announcement'}
+        />
+        <Button 
+            style={styles.button}
+            title='Post'/>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        marginTop: 10,
+        backgroundColor: '#dff5f4',
+
+    }, 
+    input: {
+      
+    },
+    button:{
+        height: 40,
+
+    },
+})
