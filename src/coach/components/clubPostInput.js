dc6e6821d49ee551b1dde01aa066def9fdf9aa29
Spyros <spyridonkaperonis@gmail.com>
2022-08-07 13:51:49 -0400

    adds clubPostInput.js component

---
+++ src/coach/components/clubPostInput.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {View, Text, StyleSheet, TextInput} from 'react-native'
+
+export const ClubPostInput = () => {
+  return (
+    <View>
+        <TextInput/>
+    </View>
+  )
+}
