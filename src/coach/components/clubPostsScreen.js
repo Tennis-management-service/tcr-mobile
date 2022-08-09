e0b24959972508552ff1fdf63d4ed5dccf32d1da
Spyros <spyridonkaperonis@gmail.com>
2022-08-09 06:30:06 -0400

    adds profle pictures and style changes

--- src/coach/components/clubPostsScreen.js
+++ src/coach/components/clubPostsScreen.js
@@ -11,8 +11,10 @@ export const ClubPostsScreen = ({props}) => {
         
         cards.push(
             <View style={styles.card}>
-
-                {data[i].name ? <Text style={styles.nameText} key={i+2}>{data[i].name}</Text> : null}
+                <View style={styles.cardHeader}>
+                    {data[i].profileImage ? <Image style={styles.profileImage} key={i+3} source={{uri: data[i].profileImage}}/>: null }
+                    {data[i].name ? <Text style={styles.nameText} key={i+2}>{data[i].name}</Text> : null}
+                </View>
                 {data[i].desc ? <Text style={styles.descText}  key={i+1}>{data[i].desc}</Text>: null}
                 {data[i].image ? <Image key={i+3} style={styles.image} source={{uri: data[i].image}}/> : null}
 
@@ -22,7 +24,7 @@ export const ClubPostsScreen = ({props}) => {
     }
   return (
 
-    <View style={[styles.container, {width: screenWidth}]}>
+    <View key={0} style={[styles.container, {width: screenWidth}]}>
         {cards}
     </View>
   )
@@ -35,6 +37,7 @@ const styles = StyleSheet.create({
     card:{
         flex:1,
         marginTop: 10,
+        marginBottom: 10,
         padding: 5,
         backgroundColor: '#dff5f4',
 
@@ -42,11 +45,25 @@ const styles = StyleSheet.create({
         borderWidth: 1,
 
     },
+    cardHeader: {
+        flex:1,
+        flexDirection: 'row',
+        alignItems: 'center',
+
+    },
+    profileImage: {
+        height: 70,
+        width: 70,
+        
+
+        //Position
+        
+    },
     nameText:{
         fontWeight: 'bold',
         fontSize: 15,
-        marginTop: 5,
-        marginBottom: 5,
+        
+        margin: 15,
     },
     descText:{
         fontSize: 15,
