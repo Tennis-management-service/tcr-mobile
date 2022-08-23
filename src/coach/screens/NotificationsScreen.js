b8fd6d95e7c7f66f7b6158bd0ecd2550ca28b1b5
Spyros <spyridonkaperonis@gmail.com>
2022-08-22 21:28:25 -0400

    add <CoachNotificationCard/> component

--- src/coach/screens/NotificationsScreen.js
+++ src/coach/screens/NotificationsScreen.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {View, Text} from 'react-native';
 
+import { CoachNotificationCards } from '../components/coachNotificationCards';
+
 
 export const NotificationsScreen = () => {
   return (
     <View>
-        <Text>This is notifications screen</Text>
+        <CoachNotificationCards />
     </View>
   );
 }
