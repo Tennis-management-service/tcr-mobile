6052b271d67759c404ecf03c6b7700fdf92a3999
Spyros <spyridonkaperonis@gmail.com>
2022-09-03 15:19:43 -0400

    adds code into NotificationsScreen.
    Renders cards from data but it still needs work because there will be more cards to display different kinds of notifications

--- src/coach/screens/NotificationsScreen.js
+++ src/coach/screens/NotificationsScreen.js
@@ -1,13 +1,80 @@
 import React from 'react'
-import {View, Text} from 'react-native';
+import {View, Text, Dimensions, StyleSheet} from 'react-native';
 
-import { CoachNotificationCards } from '../components/coachNotificationCards';
+import { CoachPostNotification } from '../components/coachNotificationCard';
+import { dummyDATA } from '../../dummyData';
+import { DummyDataPostNotifications } from '../dummyDataNotifications';
+
+export const NotificationsScreen = ({navigation}) => {
+  const screenWidth = Math.round(Dimensions.get('window').width);
+
+  // Dummy data will be replaced by Coach_notifications table render for this coach.
+  const data = DummyDataPostNotifications();
+
+  const NotificationProps = {
+    data: dummyDATA,
+    screenWidth: screenWidth,
+    image: true,
+
+  }
 
 
-export const NotificationsScreen = () => {
+  let cards = [];
+
+  for(let i=0; i<data.length; i++){
+    const props = {
+      data:data[i],
+      style: Styles,
+    }
+    cards.push(
+      
+      <View style={Styles.container}>
+        
+          <CoachPostNotification props={props}/>
+          
+      </View>
+    )
+  }
+
+
   return (
-    <View>
-        <CoachNotificationCards />
+    //For loop to loop through data
+    <View key={'14'} style={[Styles.container, {width: screenWidth}]}>
+        {cards}
     </View>
   );
 }
+
+const Styles = StyleSheet.create({
+    container:{
+      
+    
+    },
+
+
+    // CoachPostNotification Card Style bellow ----------------
+    cardContainer:{
+      flexDirection: 'row',
+      margin: 3, 
+      padding: 5, 
+      alignItems: 'center', 
+
+      backgroundColor: '#dff5f4',
+      
+    },
+    profileImage: {
+      height: 70, 
+      width: 70,
+      marginRight: 10, 
+      borderRadius: 20,
+    },
+    textContainer:{
+      flexDirection: 'row', 
+    },
+    nameText:{
+      fontWeight: 'bold',
+    },
+   
+   
+    
+})
