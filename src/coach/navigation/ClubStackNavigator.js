851396b1d0fdb484e28f5428b7e4f2d9673e0391
Spyros <spyridonkaperonis@gmail.com>
2022-09-03 15:15:58 -0400

    removes import from react-native because it is not needed here

--- src/coach/navigation/ClubStackNavigator.js
+++ src/coach/navigation/ClubStackNavigator.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet, View, TextInput, Button} from 'react-native';
 
 //Screens
 import { ClubScreen } from '../screens/ClubScreen';
