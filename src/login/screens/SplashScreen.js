1566f458b9709ab49c113028d97e7f24510ea15b
SpyridonKaperonis <spyridonkaperonis@gmail.com>
2022-09-10 16:42:03 -0400

    adds Splashscreen style and activityIndicator

--- src/login/screens/SplashScreen.js
+++ src/login/screens/SplashScreen.js
@@ -1,28 +1,36 @@
 import React from 'react'
-import { View, Text, Image, StyleSheet } from 'react-native'
+import {ActivityIndicator, View, Text, Image, StyleSheet } from 'react-native'
 
-export const SplashScreen = () => {
+export const SplashScreen = ({navigation}) => {
+    setTimeout(function(){
+        navigation.navigate('SignIn')
+    }, 1000)
   return (
     <View style={styles.container}>
-        <Text>This is SplashScreen</Text>
+        
         <Image 
             style={styles.image}
             source={{
                 uri:'https://images.unsplash.com/photo-1567226475328-9d6baaf565cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
             }}
         />
+        <ActivityIndicator size={'small'}/>
+        
     </View>
   )
 }
 
 const styles = StyleSheet.create({
     container:{
-        height: "30%",
-        marginTop: "50%", 
+        height: "100%",
         borderWidth: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center',
     },
     image:{
         height: 100,
-        width: 100
+        width: 100,
+        borderRadius: 40, 
+        marginBottom: '20%'
     }
 })
